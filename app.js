278ac85ea019c1fb461d6a3200f645ccf608c308
TuanLM <tuanvipandpro@gmail.com>
2020-11-02 21:39:33 +0700

    Add Swagger

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+// Library
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
 const jwt = require('./src/api/utils/JwtUtils')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+// Config For App
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+// Config Swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "NodeJS Project",
+            description: "NodeJS Project Demo",
+            contact: {
+              name: "TuanLM",
+              email: "tuanvipandpro@gmail.com"
+            }
+        },
+        servers: [{url: 'http://localhost:3000', description: 'Local Server'}],
+        components: {
+            securitySchemes	: {
+                name: "bearerAuth",
+                type: 'http',
+                bearerFormat: "JWT",
+                scheme: "Bearer"
+            }
+        }
+    },
+    apis: ['./src/api/routes.js']
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,10 +48,10 @@ routes(app)
 
 // Default URL
 app.get('/', (req, res) => {
-	res.send('App is running at: ' + port);
+	res.send('<h2>App is running at: ' + port + '</h2>')
 });
 
 // Listen Port
 app.listen(port, () => {
-    console.log('RESTful API server started on: ' + port)
+    console.log(`RESTful API server started on: ${port}`)
 });
--- src/api/routes.js
+++ src/api/routes.js
@@ -7,13 +7,65 @@ module.exports = function(app) {
   // API
   app
     .route('/api/products')
+  /**
+   * @swagger
+   * /api/products:
+   *  get:
+   *    summary: Get all products
+   *    responses:
+   *      '200':
+   *        description: Request Sucessfully !
+   *      '403':
+   *        description: Forbidden !!!
+   *      '500':
+   *        description: Internal Server Error !!!
+   */    
     .get(ProductController.getAll)
 
   app.route('/api/products/:productId')
+  /**
+   * @swagger
+   * /api/products/{id}:
+   *  get:
+   *    summary: Get product by id
+   *    responses:
+   *      '200':
+   *        description: Request Sucessfully !
+   *      '403':
+   *        description: Forbidden !!!
+   *      '500':
+   *        description: Internal Server Error !!!
+   */     
     .get(ProductController.getById)
+  /**
+   * @swagger
+   * /api/products/{id}:
+   *  put:
+   *    summary: Update product by id
+   *    responses:
+   *      '200':
+   *        description: Request Sucessfully !
+   *      '403':
+   *        description: Forbidden !!!
+   *      '500':
+   *        description: Internal Server Error !!!
+   */   
     .put(ProductController.updateById)
 
   // Auth
   app.route('/auth/login')
+  /**
+   * @swagger
+   * /auth/login:
+   *  post:
+   *    summary: Login
+   *    responses:
+   *      '200':
+   *        description: Request Sucessfully !
+   *      '403':
+   *        description: Forbidden !!!
+   *      '500':
+   *        description: Internal Server Error !!!
+   */     
     .post(LoginController.login)
 }
